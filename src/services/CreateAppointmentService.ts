908b11e332a709f314b4debc47ec0eceb43c6ee7
Aron Reis <aronreis2@gmail.com>
2021-03-04 00:32:07 +0000

    file create apppoiintment service ts modified

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -10,28 +10,28 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider}: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    public async execute({ date, provider }: Request): Promise<Appointment> {
+      const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
-    const appointmentDate = startOfHour(date);
+      const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(
-      appointmentDate,
-    );
+      const findAppointmentInSameDate = appointmentsRepository.findByDate(
+        appointmentDate,
+      );
 
-    if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked');
+      if (findAppointmentInSameDate) {
+        throw Error('This appointment is already booked');
+      }
+
+      const appointment = appointmentsRepository.create({
+        provider,
+        date: appointmentDate,
+      });
+
+      await appointmentsRepository.save(appointment);
+
+      return appointment;
     }
-
-    const appointment = appointmentsRepository.create({
-      provider,
-      date: appointmentDate,
-    });
-
-    await appointmentsRepository.save(appointment)
-
-    return appointment;
-  }
 }
 
 export default CreateAppointmentService;
